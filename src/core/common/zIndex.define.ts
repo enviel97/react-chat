36213fabeb897cb5622ce5c7c6287cadc01b9c20
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-12-24 12:55:02 +0700

    z index define

---
+++ src/core/common/zIndex.define.ts
@@ -0,0 +1,9 @@
+export const zIndex = Object.freeze({
+  PageLoadingShadow: 1,
+  ChannelHeader: 1000,
+  ToastContainer: 10000,
+
+  // Modal
+  BaseModelCloseButton: 100,
+  BaseModalBackdrop: 10000,
+});
