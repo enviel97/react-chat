ab0aacbc5fc5859ef24d7730d24393df9892f8c1
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-12-28 16:59:40 +0700

    change base message url api

--- src/store/repo/message/api.ts
+++ src/store/repo/message/api.ts
@@ -1,18 +1,25 @@
 import client from "@core/api";
 import { MESSAGE_GET_LIST, MESSAGE_POST } from "@store/common/repo";
 
+const messageUrl = (conversationId: string, prefix: string) =>
+  `conversations/${conversationId}${prefix}`;
+
 export const postMessage = async (req: RequestSendMessage) => {
-  const response = await client.post<any, Response<Message>>(MESSAGE_POST, {
-    conversationId: req.conversationId,
-    content: req.message,
-  });
+  const response = await client.post<any, Response<Message>>(
+    // `${req.conversationId}${MESSAGE_POST}`,
+    messageUrl(req.conversationId, MESSAGE_POST),
+    {
+      conversationId: req.conversationId,
+      content: req.message,
+    }
+  );
   if (response.data) return response;
   throw new Error("Internal Server Error");
 };
 
 export const getMessages = async (id: string) => {
   const response = await client.get<any, Response<Message[]>>(
-    `${MESSAGE_GET_LIST}/${id}`
+    messageUrl(id, MESSAGE_GET_LIST)
   );
   if (response.data) return response;
   throw new Error("Internal Server Error");
