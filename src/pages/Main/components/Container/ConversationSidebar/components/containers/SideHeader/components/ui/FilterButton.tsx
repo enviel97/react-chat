a9bdc001804776060b163097442d912420e03f3c
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2023-03-22 17:40:31 +0700

    [UI Color]: Conversation UI color change

--- src/pages/Main/components/Container/ConversationSidebar/components/containers/SideHeader/components/ui/FilterButton.tsx
+++ src/pages/Main/components/Container/ConversationSidebar/components/containers/SideHeader/components/ui/FilterButton.tsx
@@ -1,26 +1,39 @@
 import { ButtonText } from "@components/Button";
 import useAppSelector from "@hooks/useAppSelector";
+import { selectConversationType } from "@store/slices/ui";
+import { colorBrightness } from "@theme/helper/tools";
 import { FC, memo, useMemo } from "react";
+import styled from "styled-components";
 
 interface FilterButtonProps {
   text: string;
   onClick: () => void;
 }
 
-const FilterButton: FC<FilterButtonProps> = function ({ text, onClick }) {
-  const getCurrentSelect = useAppSelector((state) => state.conversation.type);
+const ButtonTextWrapper = styled(ButtonText)<{ isSelected: boolean }>`
+  & button {
+    border: 2px solid
+      ${({ isSelected, theme }) =>
+        colorBrightness(theme.surfaceColor, isSelected ? 20 : 0)};
+    background-color: ${({ isSelected, theme }) =>
+      colorBrightness(theme.surfaceColor, isSelected ? 20 : 0)};
+  }
+`;
 
-  const color = useMemo(() => {
+const FilterButton: FC<FilterButtonProps> = function ({ text, onClick }) {
+  const getCurrentSelect = useAppSelector(selectConversationType);
+
+  const isSelected = useMemo(() => {
     const filterType = text.toLowerCase() === "group" ? "group" : "direct";
-    return getCurrentSelect === filterType ? "secondary" : "surface";
+    return getCurrentSelect === filterType;
   }, [getCurrentSelect, text]);
 
   return (
-    <ButtonText
+    <ButtonTextWrapper
       className='button'
       text={text}
-      color={color}
       onClick={onClick}
+      isSelected={isSelected}
       height={"fit-content"}
     />
   );
