624e985b6fe1f20065d090318726d966e796e77c
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2023-03-27 10:40:11 +0700

    [Profile]: Init page

--- src/pages/Main/Friends/pages/Profile/index.tsx
+++ src/pages/Main/Friends/pages/Profile/index.tsx
@@ -1,7 +1,13 @@
 import { memo } from "react";
+import { FriendPageTitle } from "../../styles/FriendPage.decorate";
+import { ProfileContainer } from "./styles/Profile.decorate";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <ProfileContainer>
+      <FriendPageTitle>Profile</FriendPageTitle>
+    </ProfileContainer>
+  );
 };
 
 export default memo(Profile);
